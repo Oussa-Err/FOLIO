cb5bfddcade9572dc642c743380f44a6855e65f0
Oussa-Err <dexterlecter0@gmail.com>
2023-09-11 15:06:54 +0100

    clean up

--- app/component/About.jsx
+++ app/component/About.jsx
@@ -1,5 +1,4 @@
 "use client";
-import React from "react";
 import AnimatedSections from "../hooks/AnimatedSections";
 
 export const About = () => {
--- app/component/Hero.jsx
+++ app/component/Hero.jsx
@@ -1,6 +1,5 @@
 "use client"
 import Image from "next/image";
-import React from "react";
 import AnimatedSections from "../hooks/AnimatedSections";
 import Type from "../hooks/TypeWriter";
 
