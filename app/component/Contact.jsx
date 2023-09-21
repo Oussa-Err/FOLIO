159f053a5f296320719ddbaaf4ead3fbb767373a
Oussa-Err <dexterlecter0@gmail.com>
2023-09-21 18:11:38 +0100

    animation bug fixed

--- app/component/Contact.jsx
+++ app/component/Contact.jsx
@@ -7,17 +7,21 @@ export const Contact = () => {
 
   return (
     <section
-      ref={observerRef}
-      className={myElementVisible? `h-[40rem] gap-8 flex flex-col justify-center items-center animate-fade-slide-down`: `gap-8 flex flex-col justify-center items-center h-auto`}
+      className="h-[40rem] gap-8 flex flex-col justify-center items-center"
       id="contact"
+      ref={observerRef}
     >
       <h1
         className={myElementVisible? `text-[3rem] block relative pb-8 sm:text-[4rem] lg:text-[5rem] font-bold animate-fade-slide-down after:content-[''] after:w-full after:h-4 sm:after:h-8 after:bg-sky-800 after:absolute after:left-0 after:top-1/2 after:z-[-1] z-10 after:animate-blue-block-slide`: `text-[3rem] block relative pb-8 sm:text-[4rem] lg:text-[5rem] font-bold after:content-[''] after:w-full after:h-4 sm:after:h-8 after:bg-sky-800 after:absolute  after:left-0 after:top-1/2 after:z-[-1] z-10`}
-        ref={elementRef}
       >
         Contact
       </h1>
-      <p className="text-xl lg:w-2/5 text-center">
+      <p 
+      ref={observerRef}
+      className={
+        myElementVisible ? `ttext-xl lg:w-2/5 text-center animate-fade-out` : `text-xl lg:w-2/5 text-center`
+      }
+      >
         If you want to carry out a project or chat with me, do not hesitate to
         send me an email:&nbsp;
         <a href="mailto:erraoui.ouss@gmail.com" className="text-sky-500">
--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -3,13 +3,12 @@ import { Header } from "../component/Header";
 const Resume = () => {
   return (
     <div>
-        <Header />
-              <iframe
+      <Header />
+      <iframe
         className="w-full h-screen"
         src="/assets/curriculum-vitae-en-version.pdf"
         title="Inline Frame Example"
-        width="300"
-        height="200"
+        frameborder="0"
       >
         Resume
       </iframe>
