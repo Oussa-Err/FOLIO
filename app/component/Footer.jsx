6b6dc89ee95e860f18c12b9f856ea9bf20335123
Oussa-Err <dexterlecter0@gmail.com>
2024-04-30 18:13:44 +0100

    footer adjustement

--- app/component/Footer.jsx
+++ app/component/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = () => {
             <a href="#portfolio">Portfolio</a>
           </li>
           <li>
-            <a href="#blog">Blog</a>
+            <a href="#achievements">Achievement</a>
           </li>
         </ul>
         <small>
